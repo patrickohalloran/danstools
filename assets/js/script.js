bfcf7f19595326d88d0e211054219b0d30db2477
yellowclover <gpmaitl@gmail.com>
2015-01-16 19:32:03 -0800

    trying to get the email feature to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
 
-    //Initialize Plugin
+    /* Initialize Plugin */
     $("#owl-content").owlCarousel({
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem : true
     });
 
-    //get carousel instance data and store it in variable owl
+    /* Get carousel instance data and store it in variable owl */
     var owl = $(".owl-carousel").data('owlCarousel');
 
+    /* Initialize the page to be on the Welcome to... Dan's Tools
+      section of the carousel */
     window.onload = function() {
         owl.jumpTo(1)
     }
+
+    /* Click on the "About Us" text to jump to that part of the carousel. */
+    $("#about-us").click(function() {
+        owl.goTo(0)
+    });
+
+    /* Click on the hammer logo to jump to the home part of the carousel. */
+    $("#hammer-logo-container").click(function() {
+        owl.goTo(1)
+    });
+
+    /* Click on the "Contact" text to jump to that part of the carousel. */
+    $("#contact").click(function() {
+        owl.goTo(2)
+    });
+
 
 });
---
+++ assets/server/form_mailer.php
@@ -0,0 +1,65 @@
+<?php
+if(!isset($_POST['first_name']) || !isset($_POST['email']) !isset($_POST['message']))
+{
+  //This page should not be accessed directly. Need to submit the form.
+  echo "error; you need to submit the form!";
+}
+$first_name = $_POST['firstname'];
+$last_name = $_POST['lastname'];
+$visitor_email = $_POST['email'];
+$visitor_phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($first_name)||empty($visitor_email)||empty($message))
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'gpmaitl@gmail.com';
+$email_subject = "$subject";
+$email_body = "You have received a new message from the user $first_name.\n".
+    "Here is the message:\n $message \n\n"."Phone: $visitor_phone";
+
+$to = "gpmaitl@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thankyou.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
+?>
+
