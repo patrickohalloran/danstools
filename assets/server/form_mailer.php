84d4a9f55c9f384937a4634338d1cd974103e730
yellowclover <gpmaitl@gmail.com>
2015-01-17 18:15:03 -0800

    got the thank you page to show up. now need to figure out why emails are not being sent.

--- assets/server/form_mailer.php
+++ assets/server/form_mailer.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_POST['first_name']) || !isset($_POST['email']) !isset($_POST['message']))
+if(!isset($_POST['firstname']) || !isset($_POST['email']) || !isset($_POST['message']))
 {
   //This page should not be accessed directly. Need to submit the form.
   echo "error; you need to submit the form!";
